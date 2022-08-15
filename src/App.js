20d0047bec195414eb7ffeb73e8adeea9ccc18b5
chinhcpdev <chinhcpdev@gmail.com>
2022-08-15 19:14:23 +0700

    done Starting Create Listing Page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
             <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
         <Navbar />
       </Router>
